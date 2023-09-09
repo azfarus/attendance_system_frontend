79dabe4833d064afb2619530b69fb7dfd67c3ea6
Radib <radib@iut-dhaka.edu>
2023-09-10 05:29:47 +0600

    session prob

--- teacher_dash.js
+++ teacher_dash.js
@@ -14,3 +14,58 @@ sidebarItems.forEach((item) => {
     document.querySelector(target).classList.add("active");
   });
 });
+
+// Function to get the teacher's ID from the session
+function getSessionTeacherId() {
+  var teacherid = null;
+
+  // Make an AJAX GET request to fetch the teacher's ID from the session
+  $.ajax({
+    url: "http://localhost:8081/session/get-teacher-session-data", // Replace with your backend API endpoint
+    method: "GET",
+    async: false, // Synchronous request to wait for the response
+    success: function (data) {
+      teacherid = data; // Store the teacher's ID
+      console.log("Teacher ID:", teacherid);
+    },
+    error: function (xhr, textStatus, errorThrown) {
+      console.error("Error fetching teacher ID");
+      console.log("Status: " + xhr.status);
+      console.log("Response Text: " + xhr.responseText);
+    },
+  });
+
+  return teacherid;
+}
+
+
+// Function to fetch teacher's data using AJAX
+function fetchTeacherData() {
+  // Get the teacher's ID from the session (you might need to update this part)
+  var teacherid = getSessionTeacherId(); // Implement this function to retrieve the teacher ID from the session
+  // Make an AJAX request to fetch the teacher's data
+//   $.ajax({
+//     url: "http://localhost:8081/teacher/" + teacherid, // Replace with your backend API endpoint
+//     method: "GET",
+//     success: function (teacherData) {
+//       // Handle the successful response here
+//       console.log(teacherData);
+//       // Populate the teacher's dashboard with the retrieved data
+//       populateTeacherDashboard(teacherData);
+//     },
+//     error: function (error) {
+//       // Handle any errors here
+//       console.error("Error fetching teacher data:", error.responseText);
+//     },
+//   });
+// }
+
+// // Function to populate the teacher's dashboard with data
+// function populateTeacherDashboard(teacherData) {
+//   // Example: $("#teacherName").text(teacherData.name);
+}
+
+// Call the function to fetch teacher's data when the page loads
+$(document).ready(function () {
+  fetchTeacherData();
+});
