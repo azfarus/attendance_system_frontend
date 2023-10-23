3f99467a74f8a2dc6c4693ea28eb03709d54f5bc
Radib <radib@iut-dhaka.edu>
2023-10-23 20:58:35 +0600

    added form in studentdash

--- JS/student_dash.js
+++ JS/student_dash.js
@@ -18,6 +18,23 @@ $(document).ready(function () {
   });
 });
 
+const sidebarItems = document.querySelectorAll(".sidebar li");
+const sections = document.querySelectorAll(".section");
+
+sidebarItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    // Remove active class from all sidebar items
+    sidebarItems.forEach((item) => item.classList.remove("active"));
+    // Add active class to clicked item
+    item.classList.add("active");
+    const target = item.getAttribute("data-target");
+    // Hide all sections
+    sections.forEach((section) => section.classList.remove("active"));
+    // Show target section
+    document.querySelector(target).classList.add("active");
+  });
+});
+
 document.addEventListener("DOMContentLoaded", function () {
     var ctx = document.getElementById("myChart").getContext("2d");
   
