54e6277483cc6f6363b4aa14cb8691dca743b924
Radib <radib@iut-dhaka.edu>
2023-10-24 16:01:40 +0600

    added teacher pp

--- JS/teacher_dash.js
+++ JS/teacher_dash.js
@@ -1,216 +1,200 @@
 $(document).ready(function () {
-  sessiondata = localStorage.getItem("mysession");
-  hashdata = localStorage.getItem("myhash");
-  $.ajax({
-    url: "http://localhost:8081/session/get-session-data",
-    method: "GET",
-    headers: {
-      'mysession': sessiondata,
-      'Authorization': 'Basic ' + hashdata
-    },
-    success: function (response) {
+  // Get session data and hash from local storage
+  const sessiondata = localStorage.getItem("mysession");
+  const hashdata = localStorage.getItem("myhash");
 
-    },
-    error: function (error) {
-      // Handle any errors here
-      window.location.href = "login.html";
-    },
-  });
-});
-
-// Function to get the teacher's ID from the session
-function getSessionTeacherId() {
-  var teacherid = null;
-
-  sessiondata = localStorage.getItem("mysession");
-  hashdata = localStorage.getItem("myhash");
-  // Make an AJAX GET request to fetch the teacher's ID from the session
-  $.ajax({
-    url: "http://localhost:8081/session/get-session-data", // Replace with your backend API endpoint
-    method: "GET",
-    async: false, // Synchronous request to wait for the response
-    headers: {
-      'mysession': sessiondata,
-      'Authorization': 'Basic ' + hashdata
-    },
-    success: function (data) {
-      teacherid = data; // Store the teacher's ID
-    },
-    error: function () {
-      console.error("Error fetching teacher ID");
-    },
-  });
-
-  return teacherid;
-}
-
-
-hashdata = localStorage.getItem("myhash");
-// Function to fetch teacher's data using AJAX
-function fetchTeacherData() {
-  var tid = getSessionTeacherId();
-  console.log(tid);
-  // Make an AJAX request to fetch the teacher's data
-  $.ajax({
-    url: "http://localhost:8081/teacher/info", // Replace with your backend API endpoint
-    method: "GET",
-    headers: {
-      'mysession': sessiondata,
-      'Authorization': 'Basic ' + hashdata
-    },
-    data: {
-      teacherid: tid
-    },
-    success: function (teacherData) {
-      // Handle the successful response here
-      console.log(teacherData);
-      // Populate the teacher's dashboard with the retrieved data
-      populateTeacherDashboard(teacherData);
-    },
-    error: function (error) {
-      // Handle any errors here
-      console.error("Error fetching teacher data:", error.responseText);
-    },
-  });
-
-  $.ajax({
-    url: "http://localhost:8081/teacher/sheets",
-    method: "GET",
-    headers: {
-      'mysession': sessiondata,
-      'Authorization': 'Basic ' + hashdata
-    },
-    data: {
-      teacherId: tid
-    },
-    success: function (teacherCourses) {
-      // Assuming teacherCourses is an array of course objects
-      console.log(teacherCourses);
-  
-      // Get the course-blocks element
-      const courseBlocks = document.querySelector(".course-blocks");
-  
-      // Loop through the teacherCourses array
-      teacherCourses.forEach(course => {
-        // Create a new course block
-        const courseBlock = document.createElement("div");
-        courseBlock.className = "course";
-  
-        // Create course code element
-        const courseCode = document.createElement("div");
-        courseCode.className = "course-code";
-        courseCode.textContent = course.department + course.courseid +" "+"-"+" "+ course.section;
-  
-        // Create course name element
-        const courseName = document.createElement("div");
-        courseName.className = "course-name";
-        courseName.textContent = course.coursename;
-        courseName.style.color = "#fff398";
-  
-        // Create "View Sheet" button
-        const viewSheetButton = document.createElement("button");
-        viewSheetButton.textContent = "View Sheet";
-        viewSheetButton.style.fontWeight = "500"
-        viewSheetButton.value = course.hid; // Set the course HID as the button value
-        viewSheetButton.addEventListener("click", redirectToSheet);
-  
-        // Create "Take Attendance" button
-        const takeAttendanceButton = document.createElement("button");
-        takeAttendanceButton.textContent = "Take Attendance";
-        takeAttendanceButton.style.fontWeight = "500"
-        takeAttendanceButton.value = course.hid; // Set the course HID as the button value
-        takeAttendanceButton.addEventListener("click", redirectToAttendance);
-  
-        // Append course code, course name, "View Sheet" button, and "Take Attendance" button to the course block
-        courseBlock.appendChild(courseCode);
-        courseBlock.appendChild(courseName);
-        courseBlock.appendChild(viewSheetButton);
-        courseBlock.appendChild(takeAttendanceButton);
-  
-        // Append the course block to the course-blocks container
-        courseBlocks.appendChild(courseBlock);
-      });
-    },
-    error: function (error) {
-      console.error("Error fetching teacher data:", error.responseText);
-    },
-  });  
-}
-
-// Function to populate the teacher's dashboard with data
-function populateTeacherDashboard(teacherData) {
-  $("#t_id").text(teacherData.id);
-  $("#t_name").text(teacherData.name);
-  $("#t_mail").text(teacherData.email);
-}
-
-// Add this JavaScript code to trigger file input when clicking on the profile picture
-const profilePicture = document.getElementById("profilePicture");
-profilePicture.src = "http://localhost:8081/teacher/get-photo/" + getSessionTeacherId();
-const profilePictureInput = document.getElementById("profilePictureInput");
-
-profilePicture.addEventListener("click", function () {
-    profilePictureInput.click(); // Trigger the file input element when the profile picture is clicked
-});
-
-// Add this JavaScript code to handle file selection and update the profile picture
-profilePictureInput.addEventListener("change", function () {
-  const selectedFile = profilePictureInput.files[0]; // Get the selected file
-  const formData = new FormData();
-  formData.append("file", selectedFile);
-  const uploadUrl = "http://localhost:8081/teacher/upload-photo/" + getSessionTeacherId();
-  $.ajax({
-    url: uploadUrl,
-    type: "POST",
-    headers: {
-      'mysession': sessiondata,
-      'Authorization': 'Basic ' + hashdata
-    },
-    data: formData,
-    processData: false,
-    contentType: false,
-    success: function (data) {
-        if (data && data > 0) {
-            alert("Photo uploaded successfully.");
-        } else {
-            alert("Failed to upload photo.");
-        }
-    },
-    error: function (error) {
-        console.error("Error uploading photo:", error);
-    },
-});
-
-
-if (selectedFile) {
-      // Create a FileReader to read the selected image file
-      const reader = new FileReader();
-
-      reader.onload = function (e) {
-          // Update the profile picture's src attribute with the selected image data
-          profilePicture.src = e.target.result;
-      };
-
-      // Read the selected file as a data URL (for displaying as an image)
-      reader.readAsDataURL(selectedFile);
+  // Function to check if the user is authenticated
+  function checkAuthentication() {
+    $.ajax({
+      url: "http://localhost:8081/session/get-session-data",
+      method: "GET",
+      headers: {
+        'mysession': sessiondata,
+        'Authorization': 'Basic ' + hashdata
+      },
+      success: function () {
+        // User is authenticated
+        fetchTeacherData();
+      },
+      error: function () {
+        // Handle authentication error by redirecting to the login page
+        window.location.href = "login.html";
+      },
+    });
   }
-});
 
+  // Function to fetch the teacher's ID from the session
+  function getSessionTeacherId() {
+    let teacherId = null;
 
-// Function to redirect to the attendance sheet page
-function redirectToSheet(event) {
-  const hid = event.target.value;
-  window.location.href = "AttendanceSheet.html?hid=" + hid;
-}
+    $.ajax({
+      url: "http://localhost:8081/session/get-session-data",
+      method: "GET",
+      async: false,
+      headers: {
+        'mysession': sessiondata,
+        'Authorization': 'Basic ' + hashdata
+      },
+      success: function (data) {
+        teacherId = data;
+      },
+      error: function () {
+        console.error("Error fetching teacher ID");
+      },
+    });
 
-// Function to redirect to the attendance page
-function redirectToAttendance(event) {
-  const hid = event.target.value;
-  window.location.href = "TakeAttendance.html?hid=" + hid;
-}
+    return teacherId;
+  }
 
+  // Function to fetch teacher data and populate the dashboard
+  function fetchTeacherData() {
+    const teacherId = getSessionTeacherId();
+    const profilePicture = document.getElementById("profilePicture");
+    profilePicture.src = "http://localhost:8081/teacher/get-photo/" + teacherId;
 
-// Call the function to fetch teacher's data when the page loads
-$(document).ready(function () {
-  fetchTeacherData();
+    $.ajax({
+      url: "http://localhost:8081/teacher/info",
+      method: "GET",
+      headers: {
+        'mysession': sessiondata,
+        'Authorization': 'Basic ' + hashdata
+      },
+      data: {
+        teacherid: teacherId
+      },
+      success: function (teacherData) {
+        populateTeacherDashboard(teacherData);
+      },
+      error: function (error) {
+        console.error("Error fetching teacher data:", error.responseText);
+      },
+    });
+
+    $.ajax({
+      url: "http://localhost:8081/teacher/sheets",
+      method: "GET",
+      headers: {
+        'mysession': sessiondata,
+        'Authorization': 'Basic ' + hashdata
+      },
+      data: {
+        teacherId: teacherId
+      },
+      success: function (teacherCourses) {
+        renderCourseBlocks(teacherCourses);
+      },
+      error: function (error) {
+        console.error("Error fetching teacher data:", error.responseText);
+      },
+    });
+  }
+
+  // Function to populate the teacher's dashboard
+  function populateTeacherDashboard(teacherData) {
+    $("#t_id").text(teacherData.id);
+    $("#t_name").text(teacherData.name);
+    $("#t_mail").text(teacherData.email);
+  }
+
+  // Function to render course blocks
+  function renderCourseBlocks(courses) {
+    const courseBlocks = document.querySelector(".course-blocks");
+
+    courses.forEach(course => {
+      const courseBlock = createCourseBlock(course);
+      courseBlocks.appendChild(courseBlock);
+    });
+  }
+
+  // Function to create a course block
+  function createCourseBlock(course) {
+    const courseBlock = document.createElement("div");
+    courseBlock.className = "course";
+
+    const courseCode = document.createElement("div");
+    courseCode.className = "course-code";
+    courseCode.textContent = `${course.department} ${course.courseid} - ${course.section}`;
+
+    const courseName = document.createElement("div");
+    courseName.className = "course-name";
+    courseName.textContent = course.coursename;
+    courseName.style.color = "#fff398";
+
+    const viewSheetButton = createButton("View Sheet", course.hid, redirectToSheet);
+    const takeAttendanceButton = createButton("Take Attendance", course.hid, redirectToAttendance);
+
+    courseBlock.appendChild(courseCode);
+    courseBlock.appendChild(courseName);
+    courseBlock.appendChild(viewSheetButton);
+    courseBlock.appendChild(takeAttendanceButton);
+
+    return courseBlock;
+  }
+
+  // Function to create a button element
+  function createButton(label, value, clickHandler) {
+    const button = document.createElement("button");
+    button.textContent = label;
+    button.style.fontWeight = "500";
+    button.value = value;
+    button.addEventListener("click", clickHandler);
+    return button;
+  }
+
+  // Event listener to handle profile picture upload
+  const profilePictureInput = document.getElementById("profilePictureInput");
+  profilePicture.addEventListener("click", () => profilePictureInput.click());
+
+  profilePictureInput.addEventListener("change", handleProfilePictureUpload);
+
+  function handleProfilePictureUpload() {
+    const selectedFile = profilePictureInput.files[0];
+
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+      const uploadUrl = `http://localhost:8081/teacher/upload-photo/${getSessionTeacherId()}`;
+
+      $.ajax({
+        url: uploadUrl,
+        type: "POST",
+        headers: {
+          'mysession': sessiondata,
+          'Authorization': 'Basic ' + hashdata
+        },
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+          if (data && data > 0) {
+            alert("Photo uploaded successfully.");
+          } else {
+            alert("Failed to upload photo.");
+          }
+        },
+        error: function (error) {
+          console.error("Error uploading photo:", error);
+        },
+      });
+
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        profilePicture.src = e.target.result;
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  }
+
+  function redirectToSheet(event) {
+    const hid = event.target.value;
+    window.location.href = "AttendanceSheet.html?hid=" + hid;
+  }
+
+  function redirectToAttendance(event) {
+    const hid = event.target.value;
+    window.location.href = "TakeAttendance.html?hid=" + hid;
+  }
+
+  // Check user authentication on page load
+  checkAuthentication();
 });
